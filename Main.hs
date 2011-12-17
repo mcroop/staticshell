277e095707df1811d3efad8feb8eb10e221ec34a
Matthew Croop <matthewcroop@gmail.com>
2011-12-17 13:31:50 -0500

    dummy repl

---
+++ Main.hs
@@ -0,0 +1,42 @@
+module Main where
+
+import Data.List
+import Data.String.Utils
+
+import System.Console.Editline.Readline as EL
+
+readEvalPrintLoop :: IO ()
+readEvalPrintLoop = do
+  maybeLine <- EL.readline ">> "
+  case maybeLine of 
+    Nothing     -> return ()
+    Just "exit" -> return ()
+    Just line -> do EL.addHistory line
+                    putStrLn $ "input: " ++ (show line)
+                    readEvalPrintLoop
+
+callbackWrapper :: IO () -> EL.Callback
+callbackWrapper f = \_ _ -> f >> return 0
+
+putEmpty :: IO ()
+putEmpty = putStrLn ""
+
+stuffStr :: String -> IO ()
+stuffStr s = mapM (EL.stuffChar) s >> return ()
+
+tabComplete :: IO ()
+tabComplete = do
+  putEmpty
+  buf <- getLineBuffer
+  let x80 = (take 80) . repeat
+  putStrLn $ "Help for the function '" ++ (head $ (splitWs buf ++ [""])) ++ "'."
+  putEmpty
+  stuffStr "[added-stuff]"
+  putStrLn "completion 1\tcompletion 2"
+  EL.redisplay
+
+main :: IO ()
+main = do
+  EL.bindKey '!' (\i c -> do {return 0}) -- I'm not sure why this is necessary
+  EL.addDefun "ss-tab" (callbackWrapper tabComplete) (Just '\t')
+  readEvalPrintLoop
