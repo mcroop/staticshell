44ecb6a54e3b4952e526ab5f4e8f82da1e06dfe7
Alex Marple <admarple@gmail.com>
2011-12-18 20:57:39 -0500

    WIP Parser and instances to use HSH wrapper

--- CmdParser.hs
+++ CmdParser.hs
@@ -1,6 +1,52 @@
 module CmdParser where
 
 import Command
+import HSH
+
+tokenize :: String -> [String]
+tokenize _ = undefined
+
+splitBy :: (a -> Bool) -> [a] -> [[a]]
+splitBy _ [] = []
+splitBy f list = first : splitBy f (dropWhile f rest) where
+  (first, rest) = break f list
 
 parseLine :: String -> UntypedCommand
-parseLine _ = CmdNop
+parseLine ""     = CmdNop
+parseLine s = CmdRun (UntypedCommandData com args)
+  where
+    (com:args) = splitBy (== ' ') s
+    
+                
+{-
+execCmd :: RunResult b => UntypedCommand -> b
+execCmd CmdNop = return ()
+execCmd (CmdRun (UntypedCommandData com args)) = run (com, args)
+execCmd (CmdSeq (UntypedCommandData c1 a1) 
+               (UntypedCommandData c2 a2)) = run (c1, a1) >> run (c2, a2)
+execCmd (CmdPipe (UntypedCommandData c1 a1) 
+                (UntypedCommandData c2 a2)) = run $ (c1, a1) -|- run (c2, a2)
+
+toShellCommand :: ShellCommand b => Command a -> b
+toShellCommand CmdNop = (\_ -> return ())
+toShellCommand (CmdRun a) = case a of
+  (UntypedCommandData com args) -> (com:args)
+  (TypedCommandData com args)   -> undefined
+  _                             -> undefined
+toShellCommand (CmdSeq com1 com2) = undefined
+toShellCommand (CmdPipe com1 com2) = PipeCommand (toShellCommand com1) 
+                                                 (toShellCommand com2)
+-}
+
+{-
+type TypedCommand = Command TypedCommandData
+type UntypedCommand = Command UntypedCommandData
+-}
+                                   
+{-
+instance ShellCommand (Command TypedCommandData) where
+  fdInvoke _ _ _ = return ()
+
+instance ShellCommand (Command UntypedCommandData) where
+  fdInvoke _ _ _ = return ()
+-}
