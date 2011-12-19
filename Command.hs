5ee9f23bb3f6e81865446dd3d0e112384edf4f3e
Matthew Croop <matthewcroop@gmail.com>
2011-12-18 20:53:45 -0500

    require end-of-token whitespace

--- Command.hs
+++ Command.hs
@@ -1,7 +1,7 @@
 module Command where
 
 data ArgType = ATInt | ATString | ATFile | ATInetAddr | ATRegex
-             | ATToken String -- require the token, return ()
+             | ATToken String -- require the token, return (). the token should end in " " to require whitespace
              | ATMaybe ArgType
              | ATEither [ArgType]
              | ATSet [ArgType] -- options in any order
@@ -9,21 +9,8 @@ data ArgType = ATInt | ATString | ATFile | ATInetAddr | ATRegex
              | ATSeq [ArgType]
              | ATDocumented ArgType String
 
-
-schema :: ArgType
-schema = ATEither [
-   ATSeq [ATDocumented (ATToken "echo") "Write arguments to the standard output",
-          ATSet [ATDocumented (ATToken "-n") "Do not print trailing newline"],
-          ATList ATString],
-   ATSeq [ATDocumented (ATToken "cat") "Concatenate and print files",
-          ATSet [ATDocumented (ATToken "-b") "Number the non-blank output lines, starting at 1",
-                 ATDocumented (ATToken "-e") "Display non-printing characters (like -v) and EOL",
-                 ATDocumented (ATToken "-n") "Number the output lines, starting at 1",
-                 ATDocumented (ATToken "-s") "Squeeze multiple adjacent empty lines",
-                 ATDocumented (ATToken "-t") "Display non-printing characters (like -v) and tabs",
-                 ATDocumented (ATToken "-u") "Disable output buffering",
-                 ATDocumented (ATToken "-v") "Display non-printing characters"],
-          ATList ATFile]]
+tokWS :: ArgType
+tokWS = ATToken " "
 
 data FgBg = Fg | Bg
 
---
+++ Schema.hs
@@ -0,0 +1,33 @@
+module Schema where
+
+import Command
+
+schema :: ArgType
+schema = ATEither [
+  ATSeq [
+    ATDocumented (ATToken "echo ") "Write arguments to the standard output",
+    ATSet [
+      ATDocumented (ATToken "-n ") "Do not print trailing newline"],
+      ATList ATString],
+  ATSeq [
+    ATDocumented (ATToken "cat ") "Concatenate and print files",
+    ATSet [
+      ATDocumented (ATToken "-b ") "Number the non-blank output lines, starting at 1",
+      ATDocumented (ATToken "-e ") "Display non-printing characters (like -v) and EOL",
+      ATDocumented (ATToken "-n ") "Number the output lines, starting at 1",
+      ATDocumented (ATToken "-s ") "Squeeze multiple adjacent empty lines",
+      ATDocumented (ATToken "-t ") "Display non-printing characters (like -v) and tabs",
+      ATDocumented (ATToken "-u ") "Disable output buffering",
+      ATDocumented (ATToken "-v ") "Display non-printing characters"],
+    ATList ATFile],
+  ATSeq [
+    ATDocumented (ATToken "diff ") "Compare files line by line",
+    ATSet [
+      ATDocumented (ATEither [ATToken "-i ", ATToken "--ignore-case "]) "Ignore case differences in file contents.",
+      ATDocumented (ATEither [ATToken "-w ", ATToken "--ignore-all-space "]) "Ignore all white space.",
+      ATDocumented (ATEither [ATToken "-u ",
+                              ATSeq [ATToken "-U", ATMaybe tokWS, ATInt, tokWS],
+                              ATSeq [ATToken "--unified", ATMaybe (ATSeq [ATToken "=", ATInt]), tokWS]])
+                    "Output NUM (default 3) lines of unified context."
+    ],
+    ATList ATFile]]
