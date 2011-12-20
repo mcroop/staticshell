be67546624f924bb26994b867b185ba6449f31fd
Matthew Croop <matthewcroop@gmail.com>
2011-12-20 00:00:33 -0500

    moved documentation around in schema

--- Schema.hs
+++ Schema.hs
@@ -4,13 +4,13 @@ import Command
 
 schema :: ArgType
 schema = ATEither [
-  ATSeq [
-    ATDocumented (ATToken "echo ") "Write arguments to the standard output",
+  ATDocumented (ATSeq [
+    ATToken "echo ",
     ATSet [
       ATDocumented (ATToken "-n ") "Do not print trailing newline"],
-      ATList ATString],
-  ATSeq [
-    ATDocumented (ATToken "cat ") "Concatenate and print files",
+      ATList ATString]) "Write arguments to the standard output",
+  ATDocumented (ATSeq [
+    ATToken "cat ",
     ATSet [
       ATDocumented (ATToken "-b ") "Number the non-blank output lines, starting at 1",
       ATDocumented (ATToken "-e ") "Display non-printing characters (like -v) and EOL",
@@ -19,9 +19,9 @@ schema = ATEither [
       ATDocumented (ATToken "-t ") "Display non-printing characters (like -v) and tabs",
       ATDocumented (ATToken "-u ") "Disable output buffering",
       ATDocumented (ATToken "-v ") "Display non-printing characters"],
-    ATList ATFile],
-  ATSeq [
-    ATDocumented (ATToken "diff ") "Compare files line by line",
+    ATList ATFile]) "Concatenate and print files",
+  ATDocumented (ATSeq [
+    ATToken "diff ",
     ATSet [
       ATDocumented (ATEither [ATToken "-i ", ATToken "--ignore-case "]) "Ignore case differences in file contents.",
       ATDocumented (ATEither [ATToken "-w ", ATToken "--ignore-all-space "]) "Ignore all white space.",
@@ -30,4 +30,5 @@ schema = ATEither [
                               ATSeq [ATToken "--unified", atMaybe (ATSeq [ATToken "=", ATInt]), tokWS]])
                     "Output NUM (default 3) lines of unified context."
     ],
-    ATList ATFile]]
+    ATList ATFile]) "Write arguments to the standard output"
+  ]
